bf63e62a9a0eb30b0816d1b427901cb927169a94
giorgosmelas2 <giorgosmelas2002@gmail.com>
2024-11-12 20:31:09 +0200

    Second commit

--- moneyRecognise.m
+++ moneyRecognise.m
@@ -1,11 +1,10 @@
 data = readtable('data_banknote_authentication.txt');
-%disp(data);
 
-numRows = size(data, 1);
-randomOrder = randperm(numRows);
-
+%Suffling data
+randomOrder = randperm(size(data, 1));
 suffledData = data(randomOrder, :);
 
+%Divade data to train and test data
 n = ceil(size(data, 1)*0.7);
 
 train_data = table2array(suffledData(1:n, 1:end-1));
@@ -14,6 +13,7 @@ train_results = table2array(suffledData(1:n, end));
 test_data = table2array(suffledData(n+1:end, 1:end-1));
 test_results = table2array(suffledData(n+1:end, end));
 
+%Training model
 fis = genfis3(train_data, train_results, 'sugeno', 3);
 options = anfisOptions('InitialFIS', fis, ...
                        'EpochNumber', 500, ...
@@ -21,10 +21,18 @@ options = anfisOptions('InitialFIS', fis, ...
                        'DisplayErrorValues', true, ...
                        'DisplayStepSize', true, ...
                        'DisplayFinalResults', true);
-[trainedFIS, trainingError] = anfis(train_data, options);
+                   
+trainingDataWithLabels = [train_data, train_results];
+[trainedFIS, trainingError] = anfis(trainingDataWithLabels, options);
 
-output = evalfis(test_data, fis);
+for i = 1:length(trainedFIS.input)
+    figure;
+    plotmf(trainedFIS, 'input', i);
+    title(['Participant functions for input', num2str(i)])
+end
 
+%Rate the output
+output = evalfis(test_data, trainedFIS);
 predictedLabels = round(output);
-accuracy = sum(predictedLabels == test_results / length(test_results));
+accuracy = sum(predictedLabels == test_results) / length(test_results);
 fprintf('Accuracy: %.2f%%\n', accuracy * 100);
